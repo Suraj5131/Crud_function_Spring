7d9474df1392da9a4e1384d0e4f26d63891e8126
Suraj5131 <myemail@example.com>
2023-03-26 00:49:00 +0530

    Initial Commit

---
+++ src/test/java/com/example/Sprint_Boot_Crud/SprintBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sprint_Boot_Crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
